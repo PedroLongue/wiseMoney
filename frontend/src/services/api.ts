152128f70df1cca31e72e94a0e6a865ce0b71edc
PedroLongue <pedrolongue13@gmail.com>
2025-01-10 19:50:00 -0300

    change api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'http://localhost:3000/users',
+//  baseURL: 'http://localhost:3000/users',
+ baseURL: 'https://wise-money-blond.vercel.app/users',
 });
 
 export default api;
